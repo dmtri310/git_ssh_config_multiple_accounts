a30bf4604e8d3fb05a445046432fcdd45bdb3e62
blackrose514 <dmtri310@tutanota.com>
2021-12-21 19:48:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,73 @@
+# Git & SSH configuration for multiple github accounts
+
+Generate SSH keys
+
+```
+ssh-keygen -t ed25519 -C "my-email@example.com" #save as 'id_ed25519' (default)
+ssh-keygen -t ed25519 -C "my-second-email@example.com" #save as 'id_ed25519.user2'
+```
+
+Start SSH agent and add private keys
+
+```
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+ssh-add ~/.ssh/id_ed25519.user2
+```
+
+Add public key for each account on github
+
+```
+# Copy paste public key
+cat ~/.ssh/id_ed25519.pub
+# For second github account
+cat ~/.ssh/id_ed25519.user2.pub
+```
+
+Edit SSH config file
+
+```
+# ~/.ssh/config
+
+# User 1
+Host github.com
+        HostName github.com
+        User user1
+        IdentityFile ~/.ssh/id_ed25519
+        IdentitiesOnly yes
+        AddKeysToAgent yes
+
+# User 2
+Host github.com
+        HostName github.com-user2
+        User user2
+        IdentityFile ~/.ssh/id_ed25519_user2
+        IdentitiesOnly yes
+        AddKeysToAgent yes
+
+```
+
+Create .gitconfig for second user
+
+```
+# ~/user2.gitconfig
+
+[user]
+        user = user2
+        email = my-second-email@example.com
+[url "git@github.com-user2:user2"]
+        insteadOf = git@github.com:user2
+
+```
+
+Edit global `.gitconfig`
+
+```
+[init]
+        defaultBranch = main
+[user]
+        name = user1
+        email = my-email@example.com
+[includeIf "gitdir:~/projects/user2/"]
+        path = ~/user2.gitconfig
+```
---
+++ script.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+
+TAB="$(printf '\t')"
+
+read -p "Enter 1st user name: " user1_name
+read -p "Enter 1st user email: " user1_email
+read -p "Enter 2nd user name: " user2_name
+read -p "Enter 2nd user email: " user2_email
+
+mkdir -p "~/projects/${user1_name}"
+mkdir -p "~/projects/${user2_name}"
+
+# Generate SSH keys
+ssh-keygen -t ed25519 -C "${user1_email}" -N "" -f "~/.ssh/id_ed25519"
+ssh-keygen -t ed25519 -C "${user2_email}" -N "" -f "~/.ssh/id_ed25519.${user2_name}"
+
+# Start ssh-agent
+eval "$(ssh-agent -s)"
+
+# Add SSH private keys to ssh-agent
+ssh-add ~/.ssh/id_ed25519
+ssh-add "~/.ssh/id_ed25519.${user2_name}"
+
+# Create .gitconfig for second user
+user2_gitconfig="~/${user2_name}.gitconfig"
+touch $user2_gitconfig
+cat > $user2_gitconfig <<EOL
+[user]
+${TAB}user = ${user2_name}
+${TAB}email = ${user2_email}
+[url "git@github.com-${user2_name}:${user2_name}"]
+${TAB}insteadOf = git@github.com:${user2_name}
+EOL
+
+# Edit global .gitconfig
+touch ~/.gitconfig
+cat > ~/.gitconfig <<EOL
+[init]
+${TAB}defaultBranch = main
+[user]
+${TAB}name = ${user1_name}
+${TAB}email = ${user1_email}
+[includeIf "gitdir:~/projects/${user2_name}/"]
+${TAB}path = ${user2_gitconfig}
+EOL
+
+# Edit SSH config
+touch ~/.ssh/config
+cat > ~/.ssh/config <<EOL
+# ${user1_name}
+Host github.com
+${TAB}HostName github.com
+${TAB}User ${user1_name}
+${TAB}IdentityFile ~/.ssh/id_ed25519
+${TAB}IdentitiesOnly yes
+${TAB}AddKeysToAgent yes
+
+# ${user2_name}
+Host github.com
+${TAB}HostName github.com-${user2_name}
+${TAB}User ${user2_name}
+${TAB}IdentityFile ~/.ssh/id_ed25519.${user2_name}
+${TAB}IdentitiesOnly yes
+${TAB}AddKeysToAgent yes
+EOL
+
+echo ""
+echo "Public key for 1st user:"
+cat ~/.ssh/id_ed25519.pub
+
+echo "Public key for 2nd user:"
+cat "~/.ssh/id_ed25519.${user2_name}.pub"
